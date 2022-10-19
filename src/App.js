017089494d779d1d199531526c16e044a01f7c28
Vladislav Ustinkin <vlad.fast146@gmail.com>
2022-10-19 10:52:38 +0200

    added simple html markup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>React and Morty</h1>
+      </header>
+
+      <section>
+        <img
+          src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+          alt="Morty Smith"
+        />
+        <h2>Morty Smith</h2>
+      </section>
+
+      <nav>
+        <div>Link</div>
+        <div>Link</div>
+        <div>Link</div>
+        <div>Link</div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
